0f8c3887d889cb284e7d1093fca08643b393f0e5
tdaweetus <tdaweetus@gmail.com>
2014-02-25 20:43:00 -0600

    Create File_Upload_Form.php

---
+++ File_Upload_Form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Upload A File</title>
+</head>
+
+<body>
+<h1>Upload a file...</h1>
+<form method="post" action="upload.php" enctype="multipart/form-data" />
+<input type="file" name="myfile"  />
+<br /><br />
+<input type="submit" value="Upload File" />
+</form>
+</body>
+
+</html>
